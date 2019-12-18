4aa3e317525dbfddbb682326559599cc5217681b
visvxplore <viswa.sai@outlook.com>
2019-12-18 20:40:30 +0530

    init base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { NotesListComponent } from './components/notes-list/notes-list.component';
+import { NotesNavComponent } from './components/notes-nav/notes-nav.component';
+import { NotesEditorComponent } from './components/notes-editor/notes-editor.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, NotesListComponent, NotesNavComponent, NotesEditorComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/components/notes-nav/notes-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notes-nav',
+  templateUrl: './notes-nav.component.html',
+  styleUrls: ['./notes-nav.component.css']
+})
+export class NotesNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
